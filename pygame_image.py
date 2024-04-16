47f60ba0cdb324d9f57aaa62d506540c8f7138e7
masa <c0b230245d@edu.teu.ac.jp>
2024-04-16 17:33:51 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -23,12 +23,14 @@ def main():
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
+        elif key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, 1))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
-        if key_lst[pg.K_RIGHT]:
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-2, 0))
+        elif key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((1, 0))
+        else:
+            kk_rct.move_ip((-1, 0))
 
 
         screen.blit(bg_img, [-(tmr%3200), 0]) #練習６
