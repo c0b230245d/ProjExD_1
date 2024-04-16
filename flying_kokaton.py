46cbcda6b0db38d90e798b3c46d87beaf015fcd9
masa <c0b230245d@edu.teu.ac.jp>
2024-04-16 18:19:17 +0900

    演習２の修正

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -27,12 +27,12 @@ def main():
         elif key_lst[pg.K_DOWN]:
             dx,dy = 0,1
         elif key_lst[pg.K_LEFT]:
-            dx,dy = -2,0
-        elif key_lst[pg.K_RIGHT]:
-            dx,dy = 1,0
-        else:
             dx,dy = -1,0
-        kk_rct.move_ip([dx,dy])
+        elif key_lst[pg.K_RIGHT]:
+            dx,dy = 2,0
+        else:
+            dx,dy = 0,0
+        kk_rct.move_ip([dx-1,dy])
 
 
         screen.blit(bg_img, [-(tmr%3200), 0]) #練習６
@@ -42,7 +42,7 @@ def main():
         screen.blit(kk_img, kk_rct) #練習４
         pg.display.update()
         tmr += 1        
-        clock.tick(500)
+        clock.tick(200)
 
 
 
