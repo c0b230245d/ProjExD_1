0187dc308b691ef2676a76d8d32bad9c2f52a27a
masa <c0b230245d@edu.teu.ac.jp>
2024-04-16 16:18:11 +0900

    練習５

--- pygame_image.py
+++ pygame_image.py
@@ -18,10 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(img3, [300, 200])
+        screen.blit(img3, [300, 200]) #練習４
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
